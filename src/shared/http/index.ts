84547ca534c5e7597130cf242becd731f24a060c
AlexVSi <aleksandrsid2003@gmail.com>
2024-12-14 14:55:40 +0300

    edit Authorization header

--- src/shared/http/index.ts
+++ src/shared/http/index.ts
@@ -14,7 +14,7 @@ export const $apiAuth = axios.create({
 })
 
 $api.interceptors.request.use((config) => {
-    config.headers.Authorization = `Bearer ${localStorage.getItem('token')}`
+    config.headers.Authorization = `${localStorage.getItem('token')}`
     return config
 })
 
