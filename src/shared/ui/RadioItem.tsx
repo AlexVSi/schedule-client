833aff53ea78e8c2f85b20c050bb4ca2d49a7f46
AlexVSi <aleksandrsid2003@gmail.com>
2024-12-25 14:02:40 +0300

    fix radioItem color

--- src/shared/ui/RadioItem.tsx
+++ src/shared/ui/RadioItem.tsx
@@ -12,7 +12,7 @@ export const RadioItem: FC<RadioProps> = ({ value, item, disabled = false}) => {
     return (
         <Radio
             value={!disabled && value}
-            className={`group relative flex cursor-pointer rounded-lg bg-white/5 py-2 px-5 shadow-md transition data-[checked]:bg-white/10 ${disabled && 'bg-gray-400'}`}
+            className={`group relative flex cursor-pointer rounded-lg py-2 px-5 shadow-md transition data-[checked]:bg-white/10 ${disabled ? 'bg-gray-300' : 'bg-white/5'}`}
             disabled={disabled}
         >
             <div className="flex w-full items-center justify-between">
