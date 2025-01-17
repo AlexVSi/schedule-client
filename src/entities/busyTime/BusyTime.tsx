c0651cb16eacc362d67ca3a9c9a2941786c75cb8
AlexVSi <aleksandrsid2003@gmail.com>
2025-01-17 22:11:40 +0300

    show busy time in teacher card

--- src/entities/busyTime/BusyTime.tsx
+++ src/entities/busyTime/BusyTime.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useContext } from 'react'
+import { FC, useContext } from 'react'
 import { IBusyTime } from '@app/types/types'
 import { Card } from '@features/card/Card'
 import { Context } from 'main'
@@ -11,17 +11,18 @@ interface BusyTimeProps {
 export const BusyTime: FC<BusyTimeProps> = ({ busyTime, onDelete }) => {
     const { teacherStore, timeSlotStore } = useContext(Context)
     return (
-    <Card
-        title={`${timeSlotStore.days.find(d => d.id === busyTime.dayOfWeek)?.day}
-        ${String(new Date(busyTime.startTime).getUTCHours()).padStart(2, '0')}:${String(new Date(busyTime.startTime).getUTCMinutes()).padStart(2, '0')}
-        –
-        ${String(new Date(busyTime.endTime).getUTCHours()).padStart(2, '0')}:${String(new Date(busyTime.endTime).getUTCMinutes()).padStart(2, '0')}`}
-        onClickDelete={(e) => {
-            e?.preventDefault()
-            onDelete(busyTime.id)
-            teacherStore.removeBusyTime(busyTime.id)
-        }}
-    >
-    </Card>
+        <Card
+            title={`${timeSlotStore.days.find(d => d.id === busyTime.dayOfWeek)?.day}
+                ${String(new Date(busyTime.startTime).getUTCHours()).padStart(2, '0')}:${String(new Date(busyTime.startTime).getUTCMinutes()).padStart(2, '0')}
+                –
+                ${String(new Date(busyTime.endTime).getUTCHours()).padStart(2, '0')}:${String(new Date(busyTime.endTime).getUTCMinutes()).padStart(2, '0')}`
+            }
+            onClickDelete={(e) => {
+                e?.preventDefault()
+                onDelete(busyTime.id)
+                teacherStore.removeBusyTime(busyTime.id)
+            }}
+        >
+        </Card>
     )
 }
--- src/entities/teacher/ui/Teacher.tsx
+++ src/entities/teacher/ui/Teacher.tsx
@@ -7,13 +7,15 @@ import { ConfirmAction } from '@widgets/confirmAction/ConfirmAction'
 import { TeacherForm } from '@widgets/teacherForm/TeacherForm'
 import { Context } from 'main'
 import { observer } from 'mobx-react-lite'
+import { CardList } from '@features/cardList/CardList'
+import { BusyTime } from '@entities/busyTime/BusyTime'
 
 interface TeacherProps {
     teacher: ITeacher
 }
 
 export const Teacher: FC<TeacherProps> = observer(({ teacher }) => {
-    const { teacherStore, subjectStore } = useContext(Context)
+    const { teacherStore, subjectStore, timeSlotStore, authStore } = useContext(Context)
     const [teacherFormModal, setTeacherFormModal] = useState<boolean>(false)
     const [teacherConfirmAction, setTeacherConfirmAction] = useState<boolean>(false)
     const [subjectsForTeacher, setSubjectsForTeacher] = useState<ISubject[]>()
@@ -41,8 +43,23 @@ export const Teacher: FC<TeacherProps> = observer(({ teacher }) => {
                         <CardListItem key={subject.id} title={subject.name} />
                     ))}
                 </div>
+                {busyTimes?.length !== 0 && authStore.isAuth &&
+                <div className='flex flex-wrap'>
+                    <p className='font-medium mt-3 mb-2'>Методическое время</p>
+                    <CardList search={false}>
+                        {busyTimes?.map(bt => (
+                            <div className='p-2 border rounded'>
+                                <p>{timeSlotStore.days.find(d => d.id === bt.dayOfWeek)?.day}</p>
+                                <p>
+                                    {String(new Date(bt.startTime).getUTCHours()).padStart(2, '0')}:{String(new Date(bt.startTime).getUTCMinutes()).padStart(2, '0')}
+                                    –
+                                    {String(new Date(bt.endTime).getUTCHours()).padStart(2, '0')}:{String(new Date(bt.endTime).getUTCMinutes()).padStart(2, '0')}
+                                </p>
+                            </div>
+                        ))}
+                    </CardList>
+                </div>}
             </Card>
-
             <Modal
                 isOpen={teacherFormModal}
                 onClose={() => setTeacherFormModal(false)}
